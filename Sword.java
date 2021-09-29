d4b2acb17e80a85fa7e34a86c82e113b8b39adf7
kavyabt19 <71754877+kavyabt19@users.noreply.github.com>
2021-09-29 10:29:39 +0530

    Add files via upload

---
+++ Sword.java
@@ -0,0 +1,26 @@
+class Sword{
+
+   String killing;
+   
+   void killing()
+   {
+	   System.out.println("Inovked killing");
+	   System.out.println(killing);
+	   this.war();
+	   
+   }
+  static void inserting()
+   {
+	   System.out.println("Inovked inserting");
+	   war();
+	   
+   }
+   
+   static void war() 
+   {
+	   System.out.println("Inovked war");
+	   
+	   
+   }
+
+}
